70a1456ee6439b7e900f07b4e3e55f5f04b34eed
Caleb Maclennan <caleb@alerque.com>
2023-01-31 14:49:29 +0300

    Update demo Lua to new SILE package API
    Closes #6

--- fancy_book.lua
+++ fancy_book.lua
@@ -1,23 +1,37 @@
-SILE.require("packages/lorem")
+local base = require("packages.base")
 
-SILE.registerCommand("class:dedication", function(options, content)
-  SILE.call("hbox")
-  SILE.call("vfill")
-  SILE.call("center", {}, function()
-    SILE.call("font", { size = "20pt", style = "Italic" }, content)
+local package = pl.class(base)
+package._name = "fancy_book"
+
+function package:_init()
+  base._init(self)
+  self:loadPackage("lorem")
+end
+
+function package:registerCommands ()
+
+  self:registerCommand("class:dedication", function(_, content)
+    SILE.call("hbox")
+    SILE.call("vfill")
+    SILE.call("center", {}, function()
+      SILE.call("font", { size = "20pt", style = "Italic" }, content)
+    end)
+    SILE.call("vfill")
   end)
-  SILE.call("vfill")
-end)
 
-SILE.registerCommand("myappendix", function (options, content)
-  SILE.typesetter:typeset("Just a demo, no actual appendix")
-end)
+  self:registerCommand("myappendix", function (options, _)
+    SILE.typesetter:typeset("Just a demo, no actual appendix")
+  end)
 
-SILE.registerCommand("padpage", function (options, _)
-	local pages = SU.required(options, "pages", "padpage")
-	for i = 1, pages do
-		SILE.process({ "This page used to pad out a book length document" })
-		SILE.call("lorem", { words = 10 })
-		SILE.call("vfill")
-	end
-end)
+  self:registerCommand("padpage", function (options, _)
+    local pages = SU.required(options, "pages", "padpage")
+    for i = 1, pages do
+      SILE.process({ "This page used to pad out a book length document" })
+      SILE.call("lorem", { words = 10 })
+      SILE.call("vfill")
+    end
+  end)
+
+end
+
+return package
