0b121d20d7e892da17383c92093e906d2fdf9f99
Caleb Maclennan <caleb@alerque.com>
2023-02-07 23:56:10 +0300

    Fix GH Actions artifact uploader

--- casile.mk
+++ casile.mk
@@ -11,3 +11,7 @@ CICONFIG = .github/workflows/casile.yml
 # Default paper weight is 60g, but for this demo project we're pretending to
 # print on cardstock so the book has some heft without needing too many pages
 PAPERWEIGHT = 300
+
+# Simplify output directory to remove CaSILE's default project/branch nesting.
+# The GH Actions demo uses an artifact uploader that likes things simpler.
+DISTDIR = $(PROJECT)$(subst /,-,$(call versioninfo))
